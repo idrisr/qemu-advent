fe36149133a5a6892a92375e479a6bd469fd1fc7
Idris Raja <idris.raja@gmail.com>
2023-06-22 12:47:14 -0500

    qemu day3

--- flake.nix
+++ flake.nix
@@ -5,30 +5,54 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      prog = with pkgs;
+      day01 = with pkgs;
         stdenv.mkDerivation {
-          name = "qemu-advent-day01";
+          name = "2020 qemu-advent-day01";
           src = fetchurl {
             url =
               "https://www.qemu-advent-calendar.org/2020/download/day01.tar.gz";
             hash = "sha256-joBFhVbCqibSx2r1eb9Tyme5Rgz+MiY9vARK5HnI8VU=";
           };
-          patches = [ ./readonly.patch ];
+          patches = [ ./01patch ];
 
           buildPhase = "";
           buildInputs = [ qemu ];
           installPhase = ''
             mkdir -p $out
-            cp run.sh $out
-            cp tweetboot.img $out
-            cp adv-cal.txt $out
+            cp -r * $out
+          '';
+        };
+
+      day03 = with pkgs;
+        stdenv.mkDerivation {
+          name = "2020 qemu-advent-day03";
+          src = fetchurl {
+            url =
+              "https://www.qemu-advent-calendar.org/2020/download/gw-basic.tar.xz";
+            hash = "sha256-D1GSCLq7PytB1FI9i81toCbzlkZ57dA5iqHdLhx1T2U=";
+          };
+
+          patches = [ ./03patch ];
+          buildInputs = [ qemu ];
+          installPhase = ''
+            mkdir -p $out
+            cp -r * $out
           '';
         };
 
     in {
-      apps.${system}.day01 = {
-        program = "${prog}/run.sh";
-        type = "app";
+      apps.${system} = {
+        day01 = {
+          program = "${day01}/run.sh";
+          type = "app";
+          description = "snake game";
+        };
+        day03 = {
+          program = "${day03}/run.sh";
+          type = "app";
+        };
       };
+
+      devShells.${system} = { inherit day01 day03; };
     };
 }
